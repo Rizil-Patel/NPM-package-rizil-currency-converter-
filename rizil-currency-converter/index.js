c411b6bdc94ce77eae1fdf89e669de16d0660e4b
Rizil PATEL <128392275+Rizil-Patel@users.noreply.github.com>
2024-02-24 18:56:37 +0530

    Update index.js

--- rizil-currency-converter/index.js
+++ rizil-currency-converter/index.js
@@ -2,6 +2,7 @@ import Freecurrencyapi from '@everapi/freecurrencyapi-js';
 
 const freecurrencyapi = new Freecurrencyapi('fca_live_v0vOevL2vMwMU4pdIyX4BPIaK5dCPoODwn46CVPB');
 
+// convert fromCurrency to toCurrency units 
 export async function convertCurrency(fromCurrency,toCurrency,units) {
     const res = await freecurrencyapi.latest({
         base_currency: fromCurrency,
